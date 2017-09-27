e921244858b5f70f1c6fa17ea35975a62dafdb9d
hatoya <hatoya.web.design@gmail.com>
2017-09-27 18:08:41 +0900

    modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Http, Headers } from '@angular/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  private headers: Headers = new Headers
+  private baseURL: string = 'https://www.bungie.net/Platform'
+  public characters: any[] = []
+
+  constructor(private http: Http) {
+    this.headers.append('X-API-Key', '26afb960ed334cc09268788c92305fd6')
+  }
+
+  ngOnInit() {
+    this.http.get(this.baseURL + '/Destiny2/2/Profile/4611686018443892267/', {headers: this.headers, params: {components: '200'}}).subscribe(content => {
+      let characterObject = JSON.parse(content['_body'])['Response']['characters']['data']
+      for(let key of Object.keys(characterObject)) this.characters.push(characterObject[key])
+      console.log(this.characters[0])
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpModule } from '@angular/http'
 
-import { AppComponent } from './app.component';
+// Component
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
