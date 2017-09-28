75b028b0694809a56a05fa485e463c958593bd08
hatoya <hatoya.web.design@gmail.com>
2017-09-28 16:55:52 +0900

    modify interceptor service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,30 +5,28 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 @Injectable()
 export class ApiService {
 
-  private baseURL: string = 'https://www.bungie.net/Platform'
-
   constructor(private http: HttpClient) { }
 
   // '4611686018443892267'
   getProfile(member_id: string): Observable<any> {
     const params = new HttpParams().set('components', '100')
-    return this.http.get(this.baseURL + '/Destiny2/2/Profile/' + member_id + '/', {params: params})
+    return this.http.get('/Destiny2/2/Profile/' + member_id + '/', {params: params})
   }
 
   // '4611686018443892267'
   getCharacters(member_id: string): Observable<any> {
     const params = new HttpParams().set('components', '200')
-    return this.http.get(this.baseURL + '/Destiny2/2/Profile/' + member_id + '/', {params: params})
+    return this.http.get('/Destiny2/2/Profile/' + member_id + '/', {params: params})
   }
 
   // '4611686018443892267', '2305843009261054278'
   getCharacter(member_id: string, character_id: string) {
-    return this.http.get(this.baseURL + '/Destiny2/2/Profile/' + member_id + '/Character/' + character_id + '/')
+    return this.http.get('/Destiny2/2/Profile/' + member_id + '/Character/' + character_id + '/')
   }
 
   // 1851423
   getClanReword(clan_id: string) {
-    return this.http.get(this.baseURL + '/Destiny2/Clan/' + clan_id + '/WeeklyRewardState/')
+    return this.http.get('/Destiny2/Clan/' + clan_id + '/WeeklyRewardState/')
   }
 
 }
--- src/app/service/interceptor.service.ts
+++ src/app/service/interceptor.service.ts
@@ -5,10 +5,12 @@ import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/c
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
 
+  private baseURL: string = 'https://www.bungie.net/Platform'
+
   constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request.clone({headers: request.headers.set('X-API-Key', '26afb960ed334cc09268788c92305fd6')}))
+    return next.handle(request.clone({headers: request.headers.set('X-API-Key', '26afb960ed334cc09268788c92305fd6')}).clone({url: this.baseURL + request.url}))
   }
 
 }
