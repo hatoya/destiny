5f8d1043c77ed7e942c9ddcf3d7fbe974b09f15c
hatoya <hatoya.web.design@gmail.com>
2017-11-24 12:36:32 +0900

    modify clan-index page component

--- src/app/component/page/clan-index/clan-index.component.ts
+++ src/app/component/page/clan-index/clan-index.component.ts
@@ -20,19 +20,29 @@ export class ClanIndexComponent implements OnInit {
 
   ngOnInit() {
     this.api.getClanMembers('2027026').subscribe({
-      next: content => {
-        const contents: any[] = Object.keys(content).map(value => content[value])
-        this.members = contents
-      },
+      next: content => this.members = Object.keys(content).map(value => content[value]),
       complete: () => {
         this.state.is_load = false
         this.getElo()
+        this.getRatio()
       }
     })
   }
 
   getElo() {
-    // this.members.map(member => this.api.getElo(member).subscribe(elo => member['elo'] = elo))
+    this.members.map(member => {
+      member['gg'] = member['stats'][39] ? member['stats'][39]['elo'] : 0,
+      this.api.getTrackerElo(member['member']['destinyUserInfo']['membershipId']).subscribe(elo => member['tracker'] = elo)
+    })
+  }
+
+  getRatio() {
+    this.members.map(member => {
+      member['ratio'] = {
+        kd: member['stats'][39] ? member['stats'][39]['kills'] / member['stats'][39]['deaths'] : 0,
+        kda: member['stats'][39] ? (member['stats'][39]['kills'] + member['stats'][39]['assists']) / member['stats'][39]['deaths'] : 0
+      }
+    })
   }
 
   sortMembers() {
