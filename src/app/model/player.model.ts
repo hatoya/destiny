939ad90012384053d3cc426a8d5918cd83364d77
hatoya <hatoya.web.design@gmail.com>
2017-12-04 17:26:39 +0900

    add player model

---
+++ src/app/model/player.model.ts
@@ -0,0 +1,19 @@
+export class Player {
+
+  public id: string = ''
+  public name: string = ''
+  public elo_gg: number = 0
+  public elo_tracker: number = 0
+  public diff_gg: number = 0
+  public diff_tracker: number = 0
+  public rank_gg: number = 0
+  public rank_tracker: number = 0
+  public kd: number = 0
+  public kda: number = 0
+
+  constructor(id?: string, name?: string) {
+    this.id = id
+    this.name = name
+  }
+
+}
