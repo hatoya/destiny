cd29061d6b5d3c2dbe0d3e301069da3cb9f850f4
hatoya <hatoya.web.design@gmail.com>
2018-01-19 12:07:16 +0900

    add clan-detail page component

---
+++ src/app/component/page/clan-detail/clan-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-clan-detail',
+  templateUrl: './clan-detail.component.html',
+  styleUrls: ['./clan-detail.component.scss']
+})
+export class ClanDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
