62bd82c1855a5d09ab5438d753bf89f7a12a3b68
hatoya <hatoya.web.design@gmail.com>
2018-01-19 16:57:42 +0900

    modify enviroments

--- src/app/service/interceptor.service.ts
+++ src/app/service/interceptor.service.ts
@@ -2,15 +2,18 @@ import { Observable } from 'rxjs/Observable'
 import { Injectable } from '@angular/core'
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http'
 import { ActivatedRoute } from '@angular/router'
+import { environment } from '../../environments/environment'
 import { StorageService } from './storage.service'
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
 
   private baseURL: string = 'https://www.bungie.net/Platform'
-  private apiKey: string = '26afb960ed334cc09268788c92305fd6'
+  private apiKey: string = ''
 
-  constructor(private route: ActivatedRoute, private storage: StorageService) { }
+  constructor(private route: ActivatedRoute, private storage: StorageService) {
+    this.apiKey = environment.destiny.apiKey
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (request['url'].includes('http')) {
