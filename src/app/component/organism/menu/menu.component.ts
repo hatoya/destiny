09abdd45e8b77b3ed885a324b6c45d53fdee67c3
hatoya <hatoya.web.design@gmail.com>
2018-02-01 12:34:37 +0900

    modify menu organism component

--- src/app/component/organism/menu/menu.component.ts
+++ src/app/component/organism/menu/menu.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core'
 import { StateService } from '../../../service/state.service'
 
+interface Mode {
+  id: number
+  name: string
+}
+interface Clan {
+  id: number
+  name: string
+}
+
 @Component({
   selector: 'organism-menu',
   templateUrl: './menu.component.html',
@@ -8,9 +17,82 @@ import { StateService } from '../../../service/state.service'
 })
 export class MenuComponent implements OnInit {
 
+  public modes: Mode[] = [
+    {
+      id: 39,
+      name: 'Trials of the Nine'
+    },
+    {
+      id: 1,
+      name: 'Clash'
+    },
+    {
+      id: 2,
+      name: 'Control'
+    },
+    {
+      id: 3,
+      name: 'Supremacy'
+    },
+    {
+      id: 4,
+      name: 'Survival'
+    },
+    {
+      id: 5,
+      name: 'Countdown'
+    },
+    {
+      id: 6,
+      name: 'Iron Banner'
+    }
+  ]
+  public clans: Clan[] = [
+    {
+      id: 2027026,
+      name: '084Z'
+    },
+    {
+      id: 2140164,
+      name: 'BBJ'
+    },
+    {
+      id: 2110598,
+      name: 'Aaty'
+    },
+    {
+      id: 1157243,
+      name: 'A Knock'
+    },
+    {
+      id: 1459862,
+      name: 'Seventh Heaven'
+    },
+    {
+      id: 655503,
+      name: 'Valuser'
+    },
+    {
+      id: 2857820,
+      name: 'VioIet'
+    },
+    {
+      id: 2732118,
+      name: 'Ecla-R'
+    },
+    {
+      id: 1800744,
+      name: 'ALTEMEA'
+    }
+  ]
+
   constructor(public state: StateService) { }
 
   ngOnInit() {
+  }
+
+  changeMode(mode_id: number) {
+    this.state.mode_id = mode_id
   }
 
 }
--- src/app/service/state.service.ts
+++ src/app/service/state.service.ts
@@ -8,6 +8,7 @@ export class StateService {
   public url: string = ''
   public heading: string = ''
   public stats: any = {}
+  public mode_id: number = 39
   public is_navigation: boolean = false
   public is_load: boolean = true
   public errors: string[] = []
