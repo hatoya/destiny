ba1d675d801afd36cbeaec86ad116a49e2e7ea09
hatoya <hatoya.web.design@gmail.com>
2018-02-28 15:15:30 +0900

    modify meta service

--- src/app/service/meta.service.ts
+++ src/app/service/meta.service.ts
@@ -1,19 +1,33 @@
-import { Title } from '@angular/platform-browser'
+import { Title, Meta } from '@angular/platform-browser'
 import { Injectable } from '@angular/core'
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router'
 
 @Injectable()
 export class MetaService {
 
-  constructor(private title: Title, private router: Router) {
-    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
-      this.setTitle('')
-    })
+  public base: string = 'Destiny Clan Manager'
+
+  constructor(private title: Title, private meta: Meta, private router: Router) {
+    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => this.init())
+  }
+
+  init() {
+    this.setTitle('')
+    this.setDescription('')
+    this.meta.updateTag({ property: 'og:url', content: location.href })
   }
 
   setTitle(title: string) {
-    const base = 'Destiny Clan Manager'
-    title === '' ? this.title.setTitle(base) : this.title.setTitle(title + ' | ' + base)
+    title = title === '' ? this.base : title + ' | ' + this.base
+    this.title.setTitle(title)
+    this.meta.updateTag({ property: 'og:title', content: title })
+    this.meta.updateTag({ name: 'twitter:title', content: title })
+  }
+
+  setDescription(description: string) {
+    this.meta.updateTag({ name: 'description', content: description })
+    this.meta.updateTag({ property: 'og:description', content: description })
+    this.meta.updateTag({ name: 'twitter:description', content: description })
   }
 
 }
