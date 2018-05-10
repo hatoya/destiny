dcd5612fa17619e82990486a61e37843096a4a2a
hatoya <hatoya.web.design@gmail.com>
2018-05-10 16:35:21 +0900

    modify state service

--- src/app/service/state.service.ts
+++ src/app/service/state.service.ts
@@ -23,7 +23,7 @@ export class StateService {
   public now: Date = new Date
   public today: Date = new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate())
   public start: Date = new Date(2017, 8, 6)
-  public end: Date = this.today.getDay() < 6 ? new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 3 - this.today.getDay()) : new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 4 - this.today.getDay())
+  public end: Date = this.today.getDay() < 3 ? new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() - 4 - this.today.getDay()) : new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + 3 - this.today.getDay())
   public modes: Mode[] = [
     { id: 39, name: 'Trials of the Nine', color: '#00bcd4' },
     { id: 37, name: 'Survival', color: '#000000' },
