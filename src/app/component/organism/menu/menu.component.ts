d22bbab49dbac25ffc3e1842f3cf733579b37879
HayatoWada <hatoya.web.design@gmail.com>
2018-06-11 23:10:54 +0900

    modify menu organism component

--- src/app/component/organism/menu/menu.component.ts
+++ src/app/component/organism/menu/menu.component.ts
@@ -14,54 +14,19 @@ interface Clan {
 export class MenuComponent implements OnInit {
 
   public clans: Clan[] = [
-    {
-      id: 2027026,
-      name: '084Z'
-    },
-    {
-      id: 2140164,
-      name: 'BBJ'
-    },
-    {
-      id: 2110598,
-      name: 'Aaty'
-    },
-    {
-      id: 1157243,
-      name: 'A Knock'
-    },
-    {
-      id: 1459862,
-      name: 'Seventh Heaven'
-    },
-    {
-      id: 655503,
-      name: 'Valuser'
-    },
-    {
-      id: 3106582,
-      name: 'Freezia'
-    },
-    {
-      id: 2732118,
-      name: 'Ecla-R'
-    },
-    {
-      id: 1800744,
-      name: 'ALTEMEA'
-    },
-    {
-      id: 3052984,
-      name: 'Alternative'
-    },
-    {
-      id: 2001282,
-      name: 'cRecendo'
-    },
-    {
-      id: 3059868,
-      name: 'Large Sinner'
-    }
+    { id: 2027026, name: '084Z' },
+    { id: 2140164, name: 'BBJ' },
+    { id: 2110598, name: 'Aaty' },
+    { id: 1157243, name: 'A Knock' },
+    { id: 1459862, name: 'Seventh Heaven' },
+    { id: 655503, name: 'Valuser' },
+    { id: 3106582, name: 'Freezia' },
+    { id: 2732118, name: 'Ecla-R' },
+    { id: 1800744, name: 'ALTEMEA' },
+    { id: 3052984, name: 'Alternative' },
+    { id: 2001282, name: 'cRecendo' },
+    { id: 3059868, name: 'Large Sinner' },
+    { id: 1790239, name: 'Circus A' }
   ]
 
   constructor(public state: StateService) { }
